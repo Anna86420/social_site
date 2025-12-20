6126772832036b60661256dd3aa88d90713748c5
Anna86420 <aportnova864@gmail.com>
2025-12-20 11:20:53 +0400

    version 004

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -13,16 +13,28 @@ const Message = (props) => {
   )
 }
 const Messages = (props) => {
+  let dialogsData = [
+    {id: 1, name: 'Anna'},
+    {id: 2, name: 'Julia'},
+    {id: 3, name: 'Fedor'},
+    {id: 4, name: 'Sophiko'},
+  ]
+  let messagesData = [
+    {id: 1, message: 'Hi'},
+    {id: 2, message: 'How r u'},
+    {id: 3, message: 'Hello'},
+    {id: 4, message: 'Yo'},
+  ]
   return (
     <div className={m.dialogs}>
       <div className={m.messages_items}>
-        <DialogItem name="Anna" id="1"/>
-        <DialogItem name="Julia" id="2"/>
+        <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
+        <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
         <DialogItem name="Fedor" id="3"/>
         <DialogItem name="Sophiko" id="4"/>
       </div>
       <div className={m.messages}>
-        <Message message="Hi" />
+        <Message message={messagesData[0].message} />
         <Message message="How r u" />
         <Message message="Yo" />
       </div>
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,6 +1,10 @@
 import my from "./MyPosts.module.css";
 import Post from "./Post/Post";
 const MyPosts = () => {
+    let postsData = [
+        {id: 1, post: 'Hi, how are you?', likesCount: 10},
+        {id: 2, post: 'My first post!', likesCount: 20},
+    ]
     return (
         <div className={my.wall}>
             <h3>My posts</h3>
@@ -13,8 +17,8 @@ const MyPosts = () => {
                 </div>
 
             </div>
-            <Post message='Hi, how are you?' likes='12' />
-            <Post message='My first post!' likes='12' />
+            <Post message={postsData[0].post} likes={postsData[0].likesCount}/>
+            <Post message={postsData[1].post} likes={postsData[1].likesCount} />
         </div>
     )
 }
